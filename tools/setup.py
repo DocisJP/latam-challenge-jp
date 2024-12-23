cd43b374aa28d284f13c94f906c9c8084617b5e7
DocisJP <godoypablojuan@gmail.com>
2024-12-23 02:00:42 -0300

    feat(notebooks): Implementacion de analisis

--- tools/setup.py
+++ tools/setup.py
@@ -2,11 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="latam-challenge",
-    version="0.1",
+    version="0.9",
     packages=find_packages(),
     install_requires=[
         "pydantic>=2.0.0",
+        "pandas>=2.0.0",
+        "numpy>=1.24.0",
         "memory-profiler",
+        "emoji",
         "ijson",
     ],
+    python_requires=">=3.8",
+    author="Juan Pablo Godoy",
+    email="Godoypablojuan@gmail.com",
+    description="LATAM Data Engineering Challenge",
 ) 
